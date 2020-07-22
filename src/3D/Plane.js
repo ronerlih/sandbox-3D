6037734e66629604615c2b868197d1312d2c3482
ron erlih <ronerlih@gmail.com>
2020-07-22 13:43:21 -0400

    plane grid lines, shdow

--- src/3D/Plane.js
+++ src/3D/Plane.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import * as THREE from 'three';
 
 export default function Plane({ ...props }) {
    return (
-     <mesh {...props} receiveShadow>
-       <planeGeometry attach="geometry" args={[5000, 5000, 1, 1]} />
-       <meshLambertMaterial attach="material" color="#9b9b9b" transparent opacity={0.2} />
-     </mesh>
+      <>
+      <mesh {...props} receiveShadow>
+         <planeGeometry attach="geometry" args={[50,50,50,50]}  />
+         <meshStandardMaterial attach="material" color="white" opacity={0.7}/>
+      </mesh>
+      <mesh {...props} receiveShadow>
+         <lineSegments>
+            <wireframeGeometry attach="geometry" args={[new THREE.PlaneGeometry(50,50,50,50)]} />
+            <meshBasicMaterial attach="material" color={0x009900} />
+         </lineSegments> 
+     </mesh>     
+     </>
    )
  }
  
--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import { Canvas } from 'react-three-fiber'
+import * as THREE from 'three'
+import { Canvas, useResource } from 'react-three-fiber'
 import Controls from "./3D/OrbitControls"
 import Plane from "./3D/Plane"
 import Box from "./3D/Box"
 import './App.css';
+
+const Light = () => {
+  //Create a PointLight and turn on shadows for the light
+  const light = new THREE.DirectionalLight(0xffffff, 1, 100)
+  light.position.set(0, 10, 0)
+  light.castShadow = true // default false
+  //Set up shadow properties for the light
+  light.shadow.mapSize.width = 512 // default
+  light.shadow.mapSize.height = 512 // default
+  light.shadow.camera.near = 0.5 // default
+  light.shadow.camera.far = 500 // default
+  return <primitive object={light} />}
 
 function App() {
   return (
@@ -11,12 +24,15 @@ function App() {
       <header className="App-header">
         Sandbox
       </header>
-      <Canvas className='canvas'>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Plane rotation={[-0.5 * Math.PI, 0, 0]} position={[0, -11, 0]} />
-        <Box position={[0, 0, 0]} scale={[2,2,2]}/>
+      <Canvas 
+        className='canvas'
+        shadowMap={true}>
+        <ambientLight intensity={0.6} position={[0,10,5]} />
+        <spotLight position={[0, 4, 2]} lookAt={[0,-19,0]} castShadow={true} intensity={1}  />
+        <Plane rotation={[-0.5 * Math.PI, 0, 0]} position={[0, -2, 0]} />
+        <Box receiveShadow castShadow position={[0, 0, 0]} scale={[2,2,2]}/>
         <Controls />
+        <Light />
       </Canvas>
     </div>
   );
