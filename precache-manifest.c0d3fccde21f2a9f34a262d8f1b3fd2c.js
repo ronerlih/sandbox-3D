4e6e2615ea6f0c4c024e21460990a5a69e98f16f
ron erlih <ronerlih@gmail.com>
2020-07-23 17:01:36 -0400

    Updates

---
+++ precache-manifest.c0d3fccde21f2a9f34a262d8f1b3fd2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb7ae992311f5be5f302cec59fb3c26",
+    "url": "/2020/index.html"
+  },
+  {
+    "revision": "f9ecde5f05a4458f6da0",
+    "url": "/2020/static/css/main.c4a30d05.chunk.css"
+  },
+  {
+    "revision": "6e2918c33a2594b20fa4",
+    "url": "/2020/static/js/2.e7a16c4e.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/2020/static/js/2.e7a16c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ecde5f05a4458f6da0",
+    "url": "/2020/static/js/main.6a3921ae.chunk.js"
+  },
+  {
+    "revision": "117c3f86e3d0584d7209",
+    "url": "/2020/static/js/runtime-main.850f72d7.js"
+  }
+]);
